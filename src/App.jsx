6bfc668592731a61442a18665c6eae49d0199640
Hyunjung0221 <ekfqlc4472@naver.com>
2025-04-09 20:07:05 +0900

    ✨ Feat : 네비게이션 생성
    비로그인, 개인로그인, 기업로그인 버튼 변경

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
-import MainPage from './pages/MainPage';
+import MainPage from './pages/MainPage/MainPage';
 import UserInfoPage from './pages/UserInfoPage';
 import CompanyInfoPage from './pages/CompanyInfoPage';
 import RecruitmentInfo from './pages/RecruitmentInfo';
---
+++ src/pages/MainPage/MainPage.jsx
@@ -0,0 +1,14 @@
+// pages/Main/MainPage.jsx
+import React from 'react';
+import './MainPage.scss';
+import Header from '../../components/Header/Header.jsx';
+
+const MainPage = () => {
+    return (
+        <div className="main_page">
+            <Header />
+        </div>
+    );
+};
+
+export default MainPage;
