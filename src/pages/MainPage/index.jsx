9d7fe49fdb624b540d500533bbaa4efb8e389618
yeowun05-12 <taking0n@naver.com>
2025-04-10 02:48:29 +0900

    🚀 자동 배포 구축 완료 (NCP Object Storage + GitHub Actions)

--- src/pages/MainPage/index.jsx
+++ src/pages/MainPage/index.jsx
@@ -19,6 +19,7 @@ function MainPage() {
     <>
       <h1> 메인 페이지</h1>
       <h2>진짜 됐어...? 나 울어...?</h2>
+      <h3> main에 올려야함 </h3>
       <button onClick={handleRecruitmentClick}> 채용 정보</button>
       <button onClick={handleCompanyClick}> 기업 정보</button>
       <button onClick={handleUserClick}> 개인 정보</button>
