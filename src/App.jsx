09da3d746cc4dc1d775bb7ac4e6fd0188839e14b
dwqcwda <lty5582@naver.com>
2025-04-10 15:03:55 +0900

    ✨ Recruitmentinfo
    일반채용더미데이터+검색창+검색결과페이지

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import SignUpPage from './pages/SignUpPage';
 import UserInfoPage from './pages/UserInfoPage';
 import CompanyInfoPage from './pages/CompanyInfoPage';
 import RecruitmentInfo from './pages/RecruitmentInfo';
+import SearchResults from './pages/RecruitmentInfo/SearchResults';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
       <Route path="/user_info_page" element={<UserInfoPage />} />
       <Route path="/company_info_page" element={<CompanyInfoPage />} />
       <Route path="/recruitment_info" element={<RecruitmentInfo />} />
+      <Route path="/search-results" element={<SearchResults />} />
     </Routes>
   );
 }
