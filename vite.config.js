c9db2a9fd45819a02e3a9b2159b04afde08237d8
yeowun05-12 <taking0n@naver.com>
2025-04-10 11:58:23 +0900

    ✅[init] CI/CD 파일 구조 수정 중 - TEST01

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,20 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 // https://vite.dev/config/
 export default defineConfig({
   base: './',
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        { src: 'src/apis', dest: 'src' },
+        { src: 'src/assets', dest: 'src' },
+        { src: 'src/components', dest: 'src' },
+        { src: 'src/hooks', dest: 'src' },
+        { src: 'src/pages', dest: 'src' },
+        { src: 'src/utils', dest: 'src' },
+      ],
+    }),
+  ],
 });
