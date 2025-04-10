3e5398045cc851fb738f4e1956e1d78a22960e97
00iun <lsh681236@naver.com>
2025-04-11 00:31:07 +0900

    🔧 Chore: 더미데이터 수정
    누락 항목 추가

--- src/pages/AnnouncementAdd/index.jsx
+++ src/pages/AnnouncementAdd/index.jsx
@@ -5,6 +5,7 @@ import WorkRequirement from "../../components/Company/inputs/WorkRequirement"
 import AnnouncementContent from "../../components/Company/inputs/AnnouncementContent"
 import { useState } from "react"
 import { useNavigate } from "react-router-dom"
+import Modal from "./Modal"
 
 
 export default function AnnouncementAdd () {
@@ -13,6 +14,8 @@ export default function AnnouncementAdd () {
 
     // 더미데이터
     const [data, setData1] = useState({
+        공고제목: '',
+        근무요약: '',
         모집기간: null,
         모집기간상시: false,
         모집인원: null,
@@ -20,6 +23,9 @@ export default function AnnouncementAdd () {
         복리후생: null,
         우대조건: null,
         기타조건: null,
+        근무지주소: null,
+        근무지명: '',
+        급여: null,
         급여지급방법: null,
         근무기간: null,
         근무기간협의: false,
@@ -28,8 +34,11 @@ export default function AnnouncementAdd () {
         근무요일변동: false,
         고용형태: null,
         근무시간: null,
-        근무시간협의: null
+        근무시간협의: null,
+        공고텍스트: '',
+        이미지등록: null
     })
+    const [modal, setModal] = useState(false)
 
     return (
     <div>
@@ -42,8 +51,9 @@ export default function AnnouncementAdd () {
             <WorkRequirement data={data} setData={setData1} />
             <AnnouncementContent />
         </div>
-        <button>등록하기</button>
+        <button onClick={() => setModal(true)}>등록하기</button>
         <button>공고 미리보기</button>
+        {modal && <Modal setModal={setModal} data={data}/>}
     </div>
     )
 }
