7d02c59e1dfd3fecac9c990c48065a4c643d1229
Hyunjung0221 <ekfqlc4472@naver.com>
2025-04-10 16:05:30 +0900

    🐛 Fix : 뒤로가기 수정

--- src/components/BackHeader/index.jsx
+++ src/components/BackHeader/index.jsx
@@ -1,15 +1,12 @@
 import React from 'react';
 import './BackHeader.scss';
-import { useNavigate } from 'react-router-dom';
 import { FaArrowLeft } from 'react-icons/fa';
 
-const BackHeader = () => {
-  const navigate = useNavigate();
-
+const BackHeader = ({ onBack }) => {
   return (
     <header className="back_header">
       <div className="inner">
-        <button className="back_button" onClick={() => navigate(-1)}>
+        <button className="back_button" onClick={onBack}>
           <FaArrowLeft />
           <span className="back_text">뒤로가기</span>
         </button>
@@ -17,7 +14,7 @@ const BackHeader = () => {
           src="/logo.png"
           alt="시니어내일 로고"
           className="logo_img"
-          onClick={() => navigate('/')}
+          onClick={() => (window.location.href = '/')}
         />
       </div>
     </header>
