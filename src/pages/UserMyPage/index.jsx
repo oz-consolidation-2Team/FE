557bb7b56500dd05c90cd89196e8a64b2429d09f
yeowun05-12 <taking0n@naver.com>
2025-04-11 17:23:01 +0900

    📝 페이지 작업중

--- src/pages/UserMyPage/index.jsx
+++ src/pages/UserMyPage/index.jsx
@@ -1,8 +1,32 @@
+import { useNavigate } from 'react-router-dom';
+import JobList, { dummyData } from '../RecruitmentInfo/JobList';
+
 function UserMyPage() {
+  const navigate = useNavigate();
+
   return (
-    <>
-      <h1> 개인 마이 페이지</h1>
-    </>
+    <div className="user_mypage_container">
+      <h1>마이 페이지</h1>
+      <section className="user_info_section">
+        <div className="user_info">
+          <h2>
+            서혜진<span>님, 안녕하세요!</span>
+          </h2>
+        </div>
+
+        <button
+          onClick={() => {
+            navigate('/mypage/user/resume');
+          }}
+        >
+          간단 이력서 작성하기
+        </button>
+        <section>
+          <h3>내가 지원한 공고</h3>
+          <JobList jobs={dummyData.slice(0, 2)} />
+        </section>
+      </section>
+    </div>
   );
 }
 
