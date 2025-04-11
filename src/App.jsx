4004006518cda9ba98f2807e40137032678ef424
00iun <lsh681236@naver.com>
2025-04-11 18:18:10 +0900

    ✨ Feat: 기업 정보 확인 페이지 구성

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,12 @@ import { Route, Routes } from 'react-router-dom';
 import MainPage from './pages/MainPage';
 import SignUpPage from './pages/SignUpPage';
 import UserInfoPage from './pages/UserInfoPage';
-import CompanyInfoPage from './pages/CompanyInfoPage';
+import CompanyMyPage from './pages/CompanyMyPage';
 import RecruitmentInfo from './pages/RecruitmentInfo';
 import AnnouncementAdd from './pages/AnnouncementAdd';
 import SearchResults from './pages/RecruitmentInfo/SearchResults';
 import AnnouncementEdit from './pages/AnnouncementEdit';
+import CompanyInfo from './pages/CompanyInfo';
 
 function App() {
   return (
@@ -16,9 +17,10 @@ function App() {
       <Route path='/signup' element={<SignUpPage />} />
       <Route path="/user_info_page" element={<UserInfoPage />} />
 
-      <Route path="/company_info_page" element={<CompanyInfoPage />} />
+      <Route path="/company_my_page" element={<CompanyMyPage />} />
       <Route path="/company_announcement_add_page" element={<AnnouncementAdd />} />
       <Route path="/company_announcement_edit_page/:id" element={<AnnouncementEdit />} />
+      <Route path="/company_info_page/:id" element={<CompanyInfo />} />
 
       <Route path="/recruitment_info" element={<RecruitmentInfo />} />
       <Route path="/search-results" element={<SearchResults />} />
