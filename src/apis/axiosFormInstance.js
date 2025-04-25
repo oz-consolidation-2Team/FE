7c37e72441878b143ab8f62fc3f6592893a73153
yeowun05-12 <taking0n@naver.com>
2025-04-25 13:33:15 +0900

    📝 axiosFormInstance 파일 수정

--- src/apis/axiosFormInstance.js
+++ src/apis/axiosFormInstance.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 const axiosFormInstance = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL,
-  withCredentials: true,
+
   headers: {
     'Content-Type': 'multipart/form-data',
   },
