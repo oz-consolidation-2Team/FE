c8a992304507096b04cd76bc27a6a2164c0950ed
00iun <lsh681236@naver.com>
2025-04-29 14:49:00 +0900

    🔧 Chore: input[file] 구조 변경

--- src/components/Company/InputImage.jsx
+++ src/components/Company/InputImage.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import "./styles/InputImage.scss"
 import PropTypes from 'prop-types';
 
@@ -6,7 +7,11 @@ import PropTypes from 'prop-types';
  * @setformData 상태관리
 } */
 export default function InputImage (props) {
+    // 그저 파일명 표시하기 위함
+    const [img, setImg] = useState(null)
+
     const onchangeImageUpload = (e) => {
+        setImg(e.target.files[0].name)
         const fileName = URL.createObjectURL(e.target.files[0]);
         if (!fileName) return;
         props.setFormData((el) => ({
@@ -18,9 +23,9 @@ export default function InputImage (props) {
     return (
         <div className="InputImage_container">
             <div className="div_box">
-                <input type='file' accept = "image/*" onChange={onchangeImageUpload} />
-                <div>{props.formData.이미지등록 || "선택된 파일이 없습니다"}</div>
-                <button className="color-change">파일 선택</button>
+                <input id='input_for_button' type='file' accept = "image/*" onChange={onchangeImageUpload} />
+                <div>{img || "선택된 파일이 없습니다"}</div>
+                <label htmlFor="input_for_button" className="color-change">파일 선택</label>
             </div>
             <p>PNG, JPG ... 어쩌구 저쩌구 확장자의 파일만 등록할 수 있습니다</p>
         </div>
