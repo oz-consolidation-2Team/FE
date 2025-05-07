0f491ba1dd89e52836d1cdca93130f9944f397ae
Hyunjung0221 <ekfqlc4472@naver.com>
2025-05-08 08:46:58 +0900

    💄 Style : 기업로그인시 소셜 버튼 숨기기

--- src/pages/LoginPage/index.jsx
+++ src/pages/LoginPage/index.jsx
@@ -118,26 +118,28 @@ const LoginPage = () => {
           <span onClick={() => navigate('/forgot-password')}>비밀번호찾기</span>
         </div>
 
-        <div className="sns_login">
-        <button
-          className="kakao"
-          onClick={() => {
-            window.location.href = KAKAO_AUTH_URL;
-          }}
-        >
-          <img className="icon_kakao" src="/kakao-logo.png" alt="카카오" />
-          카카오 로그인
-        </button>
-          <button
-            className="naver"
-            onClick={() => {
-              window.location.href = NAVER_AUTH_URL;
-            }}
-          >
-            <img className="icon_naver" src="/naver-logo.png" alt="네이버" />
-            네이버 로그인
-          </button>
-        </div>
+        {userType === 'user' && (
+          <div className="sns_login">
+            <button
+              className="kakao"
+              onClick={() => {
+                window.location.href = KAKAO_AUTH_URL;
+              }}
+            >
+              <img className="icon_kakao" src="/kakao-logo.png" alt="카카오" />
+              카카오 로그인
+            </button>
+            <button
+              className="naver"
+              onClick={() => {
+                window.location.href = NAVER_AUTH_URL;
+              }}
+            >
+              <img className="icon_naver" src="/naver-logo.png" alt="네이버" />
+              네이버 로그인
+            </button>
+          </div>
+        )}
       </div>
 
       {modal && <Modal {...modal} />}
