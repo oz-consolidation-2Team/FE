85923ed983aa1ebf019711f5666f18a806860cfb
yeowun05-12 <taking0n@naver.com>
2025-05-12 12:35:33 +0900

    Fix:CICD ENV 오류 수정

--- src/pages/LoginPage/index.jsx
+++ src/pages/LoginPage/index.jsx
@@ -11,8 +11,8 @@ import { handleCompanyLogin } from '@/utils/companyLogin';
 
 const NAVER_CLIENT_ID = import.meta.env.VITE_NAVER_CLIENT_ID;
 const NAVER_REDIRECT_URI = import.meta.env.VITE_NAVER_REDIRECT_URI;
-//const STATE = 'naver_login_test';
-const NAVER_AUTH_URL = `https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=${NAVER_CLIENT_ID}&redirect_uri=${encodeURIComponent(NAVER_REDIRECT_URI)}`;
+const STATE = 'naver_login_test';
+const NAVER_AUTH_URL = `https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=${NAVER_CLIENT_ID}&redirect_uri=${encodeURIComponent(NAVER_REDIRECT_URI)}&state=${STATE}`;
 
 const KAKAO_CLIENT_ID = import.meta.env.VITE_KAKAO_CLIENT_ID;
 const KAKAO_REDIRECT_URI = import.meta.env.VITE_KAKAO_REDIRECT_URI;
