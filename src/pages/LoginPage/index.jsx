735d2ac5dd8d3340d165ab1222c899ae459f53bc
yeowun05-12 <taking0n@naver.com>
2025-05-12 12:01:11 +0900

    네이버 로그인 오류

--- src/pages/LoginPage/index.jsx
+++ src/pages/LoginPage/index.jsx
@@ -11,13 +11,12 @@ import { handleCompanyLogin } from '@/utils/companyLogin';
 
 const NAVER_CLIENT_ID = import.meta.env.VITE_NAVER_CLIENT_ID;
 const NAVER_REDIRECT_URI = import.meta.env.VITE_NAVER_REDIRECT_URI;
-const STATE = 'naver_login_test';
-const NAVER_AUTH_URL = `https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=${NAVER_CLIENT_ID}&redirect_uri=${encodeURIComponent(NAVER_REDIRECT_URI)}&state=${STATE}`;
+//const STATE = 'naver_login_test';
+const NAVER_AUTH_URL = `https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=${NAVER_CLIENT_ID}&redirect_uri=${encodeURIComponent(NAVER_REDIRECT_URI)}`;
 
 const KAKAO_CLIENT_ID = import.meta.env.VITE_KAKAO_CLIENT_ID;
 const KAKAO_REDIRECT_URI = import.meta.env.VITE_KAKAO_REDIRECT_URI;
 const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${KAKAO_CLIENT_ID}&redirect_uri=${encodeURIComponent(KAKAO_REDIRECT_URI)}`;
-
 
 const LoginPage = () => {
   const navigate = useNavigate();
@@ -73,17 +72,22 @@ const LoginPage = () => {
           </button>
         </div>
 
-        <form onSubmit={(e) => { e.preventDefault(); handleLoginClick(); }}>
-        <LabeledInput
-          label="이메일"
-          name="email"
-          value={form.email}
-          onChange={handleChange}
-          placeholder="이메일을 입력하세요"
-          error={errors.email}
-          className={userType === 'company' ? 'company_focus' : ''}
-          autoComplete="email"
-        />
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            handleLoginClick();
+          }}
+        >
+          <LabeledInput
+            label="이메일"
+            name="email"
+            value={form.email}
+            onChange={handleChange}
+            placeholder="이메일을 입력하세요"
+            error={errors.email}
+            className={userType === 'company' ? 'company_focus' : ''}
+            autoComplete="email"
+          />
 
           <div className="input_group">
             <label>비밀번호</label>
